1b4bae3cb066692b2086aca121473be39596d560
Yunwltn <120348555+Yunwltn@users.noreply.github.com>
2023-02-06 15:51:50 +0900

    다이얼로그

--- app/src/main/java/com/yunwltn98/employeeapp/adapter/EmployeeAdapter.java
+++ app/src/main/java/com/yunwltn98/employeeapp/adapter/EmployeeAdapter.java
@@ -1,8 +1,8 @@
 package com.yunwltn98.employeeapp.adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +10,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.yunwltn98.employeeapp.AddActivity;
 import com.yunwltn98.employeeapp.EditActivity;
 import com.yunwltn98.employeeapp.MainActivity;
 import com.yunwltn98.employeeapp.R;
@@ -60,7 +60,8 @@ public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.ViewHo
         TextView txtName;
         TextView txtAge;
         TextView txtSalary;
-        ImageView ingDelete;
+        ImageView imgDelete;
+        int deleteIndex;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -68,7 +69,7 @@ public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.ViewHo
             txtName = itemView.findViewById(R.id.txtName);
             txtAge = itemView.findViewById(R.id.txtAge);
             txtSalary = itemView.findViewById(R.id.txtSalary);
-            ingDelete = itemView.findViewById(R.id.ingDelete);
+            imgDelete = itemView.findViewById(R.id.imgDelete);
 
             cardView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -83,6 +84,30 @@ public class EmployeeAdapter extends RecyclerView.Adapter<EmployeeAdapter.ViewHo
 
                     // 메인 액티비티 코드 가져오기
                     ((MainActivity)context).launcher.launch(intent);
+                }
+            });
+
+            imgDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // 삭제할 인덱스 값
+                    deleteIndex = getAdapterPosition();
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    builder.setTitle("직원 정보 삭제");
+                    builder.setMessage("정말 삭제하시겠습니까");
+                    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            // 알러트 다이얼로그에서 Yes 눌렀을때 정보를 리스트에서 삭제
+                            employeeList.remove(deleteIndex);
+
+                            // 데이터가 변경되었으니 화면 갱신하라고 함수 호출
+                            notifyDataSetChanged();
+                        }
+                    });
+                    builder.setNegativeButton("NO", null);
+                    builder.show();
                 }
             });
 
